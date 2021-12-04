0b73abf827dd66d82f6606a64259ced283198067
Anthony Zhang <anthonyzhang1997@gmail.com>
2021-12-03 20:34:12 -0500

    fixed size icon

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -3,8 +3,6 @@ import { useEffect, useRef, useState } from "react";
 import { init } from 'ityped';
 import { gsap } from "gsap/all";
 import Canvas from "../Canvas/Canvas";
-import LinkedInIcon from '@mui/icons-material/LinkedIn';
-import GitHubIcon from '@mui/icons-material/GitHub';
 
 export default function Intro() {
 
@@ -15,7 +13,8 @@ export default function Intro() {
 
     useEffect(() => {
         gsap.fromTo(leftRef.current, {
-            x:'25%', opacity: 0}, {x:'50%', opacity:1, duration: 1.5}
+            x: '25%', opacity: 0
+        }, { x: '50%', opacity: 1, duration: 1.5 }
         )
     })
 
@@ -47,9 +46,15 @@ export default function Intro() {
             <Canvas id="canvas" />
             <div className="left" ref={leftRef}>
                 <div className="imgContainer">
-                    <LinkedInIcon href="https://www.linkedin.com/in/anthony-xiangyu-zhang/"></LinkedInIcon>
-                    <GitHubIcon></GitHubIcon>
-                    <i class="fab fa-stack-overflow"></i>
+                    <div className="linkedin">
+                        <a href="https://www.linkedin.com/in/anthony-xiangyu-zhang/" target="_blank" rel="noreferrer"><i class="fab fa-linkedin fa-fw"></i></a>
+                    </div>
+                    <div className="github">
+                        <a href="https://github.com/AnthonyZhang220" target="_blank" rel="noreferrer"><i class="fab fa-github fa-fw"></i></a>
+                    </div>
+                    <div className="stackoverflow">
+                        <a href="https://stackoverflow.com/users/6162027/anthony220" target="_blank" rel="noreferrer"> <i class="fab fa-stack-overflow fa-fw"></i></a>
+                    </div>
                 </div>
             </div>
             <div className="right" ref={rightRef}>
@@ -57,12 +62,16 @@ export default function Intro() {
                     <h2>Welcome, I'm</h2>
                     <h1>Anthony Zhang</h1>
                     <h3>Frontend <span ref={textRef}></span></h3>
-                    <button onClick={clicked}>Send me an Email</button>
+                    <div className="email">
+                        <button type='button' onClick={clicked}>Send me an Email</button>
+                    </div>
                 </div>
             </div>
-            <a href="#portfolio">
-                <i className="fas fa-chevron-down" ref={arrowdownRef}></i>
-            </a>
+            <div className="arrowdown">
+                <a href="#portfolio">
+                    <i className="fas fa-chevron-down" ref={arrowdownRef}></i>
+                </a>
+            </div>
         </div>
     )
 }
