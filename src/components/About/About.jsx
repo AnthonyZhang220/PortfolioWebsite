f6b734ca8062fb9e53fc906ac5f82c86d852a517
AnthonyZhang220 <anthonyzhang1997@gmail.com>
2021-12-26 20:06:25 -0500

    add about me page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -3,7 +3,12 @@ import React from 'react'
 export default function About() {
     return (
         <div className='about' id="about">
-
+            <div className="title">
+                <h1>
+                    About Me
+                </h1>
+            </div>
+            <div></div>
         </div>
     )
 }
