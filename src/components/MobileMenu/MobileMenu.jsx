b089cbb25297f0503843771510bba50232f17b8a
AnthonyZhang220 <anthonyzhang1997@gmail.com>
2022-02-12 03:30:29 -0500

    .

--- src/components/MobileMenu/MobileMenu.jsx
+++ src/components/MobileMenu/MobileMenu.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import Divider from '@mui/material/Divider';
+import ListItem from '@mui/material/ListItem';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
 
-export default function MenuBar() {
+export default function MobileMenu() {
     return (
-        <div>
-            
-        </div>
+
     )
 }
